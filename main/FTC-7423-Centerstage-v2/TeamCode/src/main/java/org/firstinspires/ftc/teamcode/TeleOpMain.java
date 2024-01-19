1a711337a4e86438254bb281a68ddaf259502638
Luke Zhao <pasqalup25@gmail.com>
2024-01-18 19:52:59 -0500

    Fix Linkage arm(actually gets to the right position on the robot!) and added height calculations to Teleop

--- main/FTC-7423-Centerstage-v2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ main/FTC-7423-Centerstage-v2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -3,14 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.utility.AxonServo;
 import org.firstinspires.ftc.teamcode.utility.Claw;
 import org.firstinspires.ftc.teamcode.utility.FieldCentricDrive;
 import org.firstinspires.ftc.teamcode.utility.LinkageArm;
 
+import java.util.concurrent.TimeUnit;
+
 @TeleOp
 public class TeleOpMain extends OpMode {
+    ElapsedTime et;
     FieldCentricDrive drive;
     Claw claw;
     AxonServo armServo;
@@ -18,8 +23,11 @@ public class TeleOpMain extends OpMode {
     PerfectPixelPlacement perfectPixelPlacement;
     DistanceSensor fc;
     boolean debug = true;
+    double height = 203;
+    int tolerance = 50;
     @Override
     public void init() {
+        et = new ElapsedTime();
         fc = hardwareMap.get(DistanceSensor.class, "fc");
         drive = new FieldCentricDrive(hardwareMap);
         claw = new Claw(hardwareMap.servo.get("claw"),0,0,0,debug);
@@ -41,13 +49,33 @@ public class TeleOpMain extends OpMode {
         }else if(gamepad2.b){
             claw.halfOpen();
         }
-
+        double currentTime = et.time(TimeUnit.MILLISECONDS);
+        if(-gamepad2.left_stick_y>0.1){
+            if((currentTime % 50) < 50){
+                height += 3;
+            }
+        }else if(-gamepad2.left_stick_y<-0.1){
+            if((currentTime % 50) < 50){
+                height -= 3;
+            }
+        }
+        if(gamepad2.left_bumper){
+            tolerance = 10;
+        }else{
+            tolerance = 50;
+        }
         // ARM LOGIC  // ARM LOGIC  // ARM LOGIC  // ARM LOGIC  //
         if(gamepad2.right_trigger>0.1){
-            //perfectPixelPlacement.executeWithSensorSpeededArm();
+            perfectPixelPlacement.executeWithSensorSpeededArm(height);
         }
         // DRIVE //
         drive.drive(gamepad1);
+
+
+        //TELEMETRY //
+        telemetry.addData("height",height);
+        telemetry.addData("distance",fc.getDistance(DistanceUnit.MM));
+        telemetry.addData("time",currentTime);
         telemetry.update();
     }
 }
--- main/FTC-7423-Centerstage-v2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/LinkageArm.java
+++ main/FTC-7423-Centerstage-v2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/LinkageArm.java
@@ -67,7 +67,7 @@ public class LinkageArm {
                         /(2*bar1len*len)
         );
         //servo range 300 degrees
-        motor.setPosition((Math.toDegrees(angle)+160)/180);
+        motor.setPosition((160-Math.toDegrees(angle))/180);
     }
     /**
      * Get the length of the linkage arm
